a6e129691c1232b9cd28494f6f65eba42a9bc1f6
Marc-André Cardinal <cardinalmarca@gmail.com>
2020-01-11 20:35:09 -0600

    Initial Commit

---
+++ Dog app/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Dog app
+//
+//  Created by Marc-André Cardinal on 2020-01-11.
+//  Copyright © 2020 marc and chub. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
